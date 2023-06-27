ad4e9875af78bcca80a26989178f0cd65053b80b
Lucas Pirez <lucaspirez42@gmail.com>
2023-06-27 16:29:38 -0300

    create component with links projects

---
+++ src/components/projects/Links.js
@@ -0,0 +1,21 @@
+import externalLink from '../../icons/external-link.svg'
+import gitHub from '../../icons/433-github.svg'
+
+export default function Links({ links }) {
+  return (
+    <>
+      <a href={links.repositorie} target="_blank" rel="noreferrer">
+        <img src={gitHub} alt="git" className="hover:scale-105" />
+      </a>
+      {links?.deploy && (
+        <a href={links.deploy} target="_blank" rel="noreferrer">
+          <img
+            src={externalLink}
+            alt="link"
+            className="hover:scale-110 transition-all"
+          />
+        </a>
+      )}
+    </>
+  )
+}
