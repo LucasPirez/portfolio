f43733fc16608ff0c8bf1693a931ae24c1402b3d
Lucas Pirez <lucaspirez42@gmail.com>
2023-06-27 16:31:51 -0300

    slint changes

--- src/translation.js
+++ src/translation.js
@@ -1,126 +1,126 @@
-import { modalEN, modalES } from "./textoModal";
+import { modalEN, modalES } from './textoModal'
 
 export const recursos = {
   projects: {
     title: {
-      ReactNative: "React Native",
-      Django: "Django",
-      React: "React-Js",
-    },
-  },
-};
+      ReactNative: 'React Native',
+      Django: 'Django',
+      React: 'React-Js'
+    }
+  }
+}
 
 export const translation = {
   en: {
-    buttonProjects: "Read More",
-    tecnologies: "TECNOLOGIES",
-    projects: "Projects",
-    theme: "Theme",
-    light: "Light",
-    dark: "Dark",
-    default: "default",
-    all: "All",
+    buttonProjects: 'Read More',
+    tecnologies: 'TECNOLOGIES',
+    projects: 'Projects',
+    theme: 'Theme',
+    light: 'Light',
+    dark: 'Dark',
+    default: 'default',
+    all: 'All',
     header: {
-      home: "Home",
-      about: "About",
-      portfolio: "Portfolio",
-      contact: "Contact",
+      home: 'Home',
+      about: 'About',
+      portfolio: 'Portfolio',
+      contact: 'Contact'
     },
     presentation: {
-      title: "Hello, I'm ",
-      subTitle: "I'm  full-stack web developer.",
-      button: "View my work.",
+      title: "Hello, I'm",
+      subTitle: 'Web and Mobile developer.',
+      button: 'View my work.'
     },
     contact: {
-      name: "Name",
-      email: "Email",
-      message: "Message",
-      submit: "Submit",
+      name: 'Name',
+      email: 'Email',
+      message: 'Message',
+      submit: 'Submit',
       errorDescription: {
-        valueMissing: "This field cannot be empty",
-        typeMismatch: "The field is incorrect",
-      },
+        valueMissing: 'This field cannot be empty',
+        typeMismatch: 'The field is incorrect'
+      }
     },
     modal: modalEN,
-    deploy: "View Deploy",
-    repositorie: "View Repositorie",
+    deploy: 'View Deploy',
+    repositorie: 'View Repositorie',
     about: {
       about:
         "Hi I'm lucas, I'm a full-stack programming student having more knowledge in front-end and specializing with Reactjs, I'm constantly trying to improve technical and soft skills, giving high priority to getting a job opportunity.",
       description1: {
-        title: "Responsive Design",
+        title: 'Responsive Design',
         description:
-          "High priority to the adaptation of the design according to the display",
+          'High priority to the adaptation of the design according to the display'
       },
       description2: {
-        title: "Intuitive",
-        description: "Simple and intuitive designs for the user",
+        title: 'Intuitive',
+        description: 'Simple and intuitive designs for the user'
       },
       description3: {
-        title: "Fast",
-        description: "fast designs without compromising quality",
+        title: 'Fast',
+        description: 'fast designs without compromising quality'
       },
       description4: {
-        title: "Teamwork",
+        title: 'Teamwork',
         description:
-          "Willingness to actively participate in a common goal, subordinating personal interests to the team's objectives.",
-      },
-    },
+          "Willingness to actively participate in a common goal, subordinating personal interests to the team's objectives."
+      }
+    }
   },
 
   es: {
-    buttonProjects: "Ver Mas",
-    tecnologies: "TECNOLOGIAS",
-    projects: "Proyectos",
-    theme: "Tema",
-    light: "Claro",
-    dark: "Oscuro",
-    default: "Predeterminado",
-    all: "Todos",
+    buttonProjects: 'Ver Mas',
+    tecnologies: 'TECNOLOGIAS',
+    projects: 'Proyectos',
+    theme: 'Tema',
+    light: 'Claro',
+    dark: 'Oscuro',
+    default: 'Predeterminado',
+    all: 'Todos',
     header: {
-      home: "Inicio",
-      about: "Acerca",
-      portfolio: "Portafolio",
-      contact: "Contacto",
+      home: 'Inicio',
+      about: 'Acerca',
+      portfolio: 'Portafolio',
+      contact: 'Contacto'
     },
     presentation: {
-      title: "Hola, soy ",
-      subTitle: " full-stack web developer.",
-      button: "ver mis proyectos.",
+      title: 'Hola, soy ',
+      subTitle: 'Desarrollador Web y Movil',
+      button: 'ver mis proyectos.'
     },
     contact: {
-      name: "Nombre",
-      email: "Email",
-      message: "Mensaje",
-      submit: "Enviar",
+      name: 'Nombre',
+      email: 'Email',
+      message: 'Mensaje',
+      submit: 'Enviar',
       errorDescription: {
-        valueMissing: "Este Campo no puede estar vacio",
-        typeMismatch: "El campo es incorrecto",
-      },
+        valueMissing: 'Este Campo no puede estar vacio',
+        typeMismatch: 'El campo es incorrecto'
+      }
     },
     modal: modalES,
-    deploy: "Ver Despliegue",
-    repositorie: "Ver Repositorio",
+    deploy: 'Ver Despliegue',
+    repositorie: 'Ver Repositorio',
     about: {
       about:
-        "Hola soy lucas, soy estudiante de programación full-stack  teniendo mayores conocimientos en el front-end y especializándome con Reactjs, me encuentro constantemente tratando de mejorar las habilidades técnicas y blandas, dándole gran prioridad a la obtención de una oportunidad de empleo.",
+        'Desarrollador full-stack con experiencia practica en diversos proyectos, especializado en Reactjs, actualmente en busqueda de un trabajo donde pueda demostrar mi determinacion y habilidades.',
       description1: {
-        title: "Diseño Responsivo",
-        description: "Gran prioridad a la adaptacion del diseño segun pantalla",
+        title: 'Diseño Responsivo',
+        description: 'Gran prioridad a la adaptacion del diseño segun pantalla'
       },
       description2: {
-        title: "Intuitivo",
-        description: "Diseños simples y intuitivos para el usuario",
+        title: 'Intuitivo',
+        description: 'Diseños simples y intuitivos para el usuario'
       },
       description3: {
-        title: "Rapido",
-        description: "diseños rapidos sin dejar de lado la calidad",
+        title: 'Rapido',
+        description: 'diseños rapidos sin dejar de lado la calidad'
       },
       description4: {
-        title: "Trabajo en equipo",
+        title: 'Trabajo en equipo',
         description:
-          "Predisposicion a participar activamente en una meta común subordinando los intereses personales a los objetivos del equipo",
-      },
-    },
-  },
-};
+          'Predisposicion a participar activamente en una meta común subordinando los intereses personales a los objetivos del equipo'
+      }
+    }
+  }
+}
