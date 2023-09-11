2e220256559055ef8df302be5d482a6797303389
Lucas Pirez <lucaspirez42@gmail.com>
2023-09-11 19:43:02 -0300

    Format code and changes styles

--- src/components/About/Tecnologis.js
+++ src/components/About/Tecnologis.js
@@ -1,6 +1,6 @@
-import { useContext, useEffect, useRef, useState } from 'react'
+import { useContext } from 'react'
 import style from './tecnologies.module.css'
-import {
+import Express, {
   Css3,
   Html,
   Reactjs,
@@ -9,29 +9,30 @@ import {
   Java
 } from '../../icons/IconsTecnologies'
 import TranslationContext from '../../TraslationContext'
-import { useInsertionEffect } from '../../hooks/useIntersection'
 
 const Tecnologies = () => {
-  const [activeStop, seTActiveStop] = useState(true)
   const { text } = useContext(TranslationContext)
-  const ref = useRef()
 
-  const hidden = (value) => {
-    value === 'tecnologies' ? seTActiveStop(true) : seTActiveStop(false)
-  }
-
-  useInsertionEffect([ref], hidden)
+  const arrCompontents = [
+    { style: style.a, component: <Html />, text: 'HTML' },
+    { style: style.b, component: <Css3 />, text: 'CSS:' },
+    { style: style.c, component: <Javascript />, text: 'JavaScript' },
+    { style: style.d, component: <TypeScript />, text: 'TypeScript' },
+    { style: style.e, component: <Java />, text: 'Java' },
+    { style: style.f, component: <Reactjs />, text: 'React' },
+    { style: style.f, component: <Reactjs />, text: 'React Native' },
+    { style: style.g, component: <Express />, text: 'Express' }
+  ]
 
   return (
     <div
       className="h-full animate-[startRight_1.2s_ease-out]"
       title="tecnologies"
-      ref={ref}
     >
       <div className={style.cuadrado1}>
         <div className={style.cuadrado}></div>
       </div>
-      <h3 className="text-center lg:text-6xl sm:text-4xl text-3xl  font-bold bg-clip-text text-transparent bg-gradient-to-bl from-slate-500 to-slate-800  m-4 dark:from-cyan-400 dark:to-cyan-600">
+      <h3 className="text-center lg:text-6xl sm:text-4xl text-3xl  font-bold bg-clip-text text-transparent  m-4 dark:text-myCyanDark/80 text-myBgBlue">
         {text.tecnologies}
       </h3>
       <div className="p-1">
@@ -39,65 +40,15 @@ const Tecnologies = () => {
           className={` ${style.tecnologi} grid xl:block grid-cols-2 sm:grid-cols-4  transition-all
         text-center lg:w-[700px] xl:w-[300px] h-auto   sm:text-lg text-xs sm:p-2  p-1   rounded `}
         >
-          <li
-            className={`${style.a} ${
-              !activeStop && style.pausarAnimacion
-            }  dark:shadow-xl dark:shadow-slate-700 `}
-          >
-            <Html /> HTML
-          </li>
-          <li
-            className={`${style.b} ${
-              !activeStop && style.pausarAnimacion
-            } dark:shadow-xl dark:shadow-slate-700`}
-          >
-            <Css3 />
-            CSS:
-          </li>
-          <li
-            className={`${style.c} ${
-              !activeStop ? style.pausarAnimacion : ''
-            } dark:shadow-xl dark:shadow-slate-700`}
-          >
-            <Javascript />
-            Javsascript
-          </li>
-          <li
-            className={`${style.d} ${
-              !activeStop ? style.pausarAnimacion : ''
-            } dark:shadow-xl dark:shadow-slate-700`}
-          >
-            <TypeScript /> TypeScript
-          </li>
-          <li
-            className={`${style.d} ${
-              !activeStop ? style.pausarAnimacion : ''
-            } dark:shadow-xl dark:shadow-slate-700`}
-          >
-            <Java /> Java
-          </li>
-
-          <li
-            className={`${style.e} ${
-              !activeStop ? style.pausarAnimacion : ''
-            } dark:shadow-xl dark:shadow-slate-700`}
-          >
-            <Reactjs /> React
-          </li>
-          <li
-            className={`${style.f} ${
-              !activeStop ? style.pausarAnimacion : ''
-            } dark:shadow-xl dark:shadow-slate-700`}
-          >
-            <Reactjs /> React Native
-          </li>
-          <li
-            className={`${style.g} ${
-              !activeStop ? style.pausarAnimacion : ''
-            } dark:shadow-xl dark:shadow-slate-700`}
-          >
-            Express
-          </li>
+          {arrCompontents.map((tecnology) => (
+            <li
+              key={tecnology.text + Math.random()}
+              className={`${tecnology.style} flex items-center pl-[4%]  dark:shadow-xl dark:shadow-myBgDark/60 dark:bg-[#0099ff70] border-2 dark:border-[#0099ff50] border-dashed border-myBgDark/70 text-myRose lg:text-xl jus `}
+            >
+              <span>{tecnology.component}</span>
+              <span className="w-full">{tecnology.text}</span>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
