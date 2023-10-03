753ffed34a214c8dcb0f032a86d01c7f8bc7e7b7
Lucas Pirez <lucaspirez42@gmail.com>
2023-10-03 20:25:54 -0300

    changging how themes are applied

--- src/components/About/Theme.js
+++ src/components/About/Theme.js
@@ -1,12 +1,14 @@
-import { useState, useEffect, useContext } from 'react'
+import { useState, useEffect } from 'react'
 import Sun from '../../icons/Sun'
 import Moon from '../../icons/Moon'
-import TranslationContext from '../../TraslationContext'
+
+const themes = {
+  light: 'dark',
+  dark: 'os',
+  os: 'light'
+}
 
 export default function Theme({ setChangeTheme }) {
-  const { text } = useContext(TranslationContext)
-  const [bool, setBool] = useState(false)
-
   const init = localStorage.theme
     ? localStorage.theme
     : window.matchMedia('(prefers-color-scheme: dark)').matches
@@ -16,7 +18,8 @@ export default function Theme({ setChangeTheme }) {
 
   function handleClick(e) {
     e.preventDefault()
-    setBool(!bool)
+
+    setSelect(themes[select])
   }
 
   useEffect(() => {
@@ -37,64 +40,25 @@ export default function Theme({ setChangeTheme }) {
         document.documentElement.classList.remove('dark')
       }
     }
+
     setChangeTheme(Math.random())
   }, [select])
 
   return (
     <>
-      <div className="relative text-slate-200">
-        <button
-          className="bg-zinc-900 rounded-2xl px-2 hover:scale-105 transition-all duration-300 h-8 border-2 border-cyan-800 z-30 "
-          onClick={handleClick}
-        >
-          {select === 'os' ? (
-            <span className="text-cyan-400">OS</span>
-          ) : select === 'light' ? (
-            <Sun />
-          ) : (
-            <Moon />
-          )}
-          <span className="mx-2 hidden lg:inline-block">{text.theme}</span>
-        </button>
-        <ul
-          className={`${
-            !bool ? 'scale-0 absolute' : ' relative scale-100'
-          } transition-all duration-300 origin-top-left bg-slate-800 dark:bg-zinc-900 p-2 rounded-md text-base border-2 border-slate-400 `}
-        >
-          <li>
-            <button
-              className="dark:bg-zinc-900  bg-slate-800 px-2 py-1 mt-1 rounded-xl border-2 w-[100%] border-cyan-700"
-              onClick={() => setSelect('os')}
-            >
-              <span className="text-cyan-400 mr-2">OS</span>
-              {text.default}
-            </button>
-          </li>
-          <li>
-            <button
-              onClick={() => setSelect('light')}
-              className="dark:bg-zinc-900 bg-slate-800  px-2 py-1 mt-1 rounded-xl border-2 w-[100%] border-cyan-700"
-            >
-              <Sun /> <span>{text.light}</span>
-            </button>
-          </li>
-          <li>
-            <button
-              className="dark:bg-zinc-900  bg-slate-800 px-2 py-1 mt-1 rounded-xl border-2 w-[100%] border-cyan-700"
-              onClick={() => setSelect('dark')}
-            >
-              <Moon /> <span>{text.dark}</span>
-            </button>
-          </li>
-        </ul>
-      </div>
-
-      {bool && (
-        <div
-          className="h-[100vh] w-[100vw] -ml-10 top-0 -z-10 absolute"
-          onClick={handleClick}
-        ></div>
-      )}
+      <button
+        className="bg-zinc-900 rounded-2xl px-2 hover:scale-105 transition-all duration-300 h-8 border-2 border-cyan-800 z-30 "
+        onClick={handleClick}
+      >
+        {select === 'os' ? (
+          <span className="text-cyan-400">OS</span>
+        ) : select === 'light' ? (
+          <Sun />
+        ) : (
+          <Moon />
+        )}
+        {/* <span className="mx-2 hidden lg:inline-block">{text.theme}</span> */}
+      </button>
     </>
   )
 }
--- src/icons/Sun.js
+++ src/icons/Sun.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react'
 
 const Sun = (props) => (
   <svg
@@ -18,12 +18,15 @@ const Sun = (props) => (
         <path d="M10.5 14.5c2.219 0 4-1.763 4-3.982a4.003 4.003 0 0 0-4-4.018c-2.219 0-4 1.781-4 4 0 2.22 1.781 4 4 4zM4.136 4.136 5.55 5.55M15.45 15.45l1.414 1.414M1.5 10.5h2M17.5 10.5h2M4.136 16.864 5.55 15.45M15.45 5.55l1.414-1.414M10.5 19.5v-2M10.5 3.5v-2" />
       </g>
       <g transform="translate(-210 -1)">
-        <path d="M220.5 2.5v2M227 5l-1.5 1.5" />
+        <path d="M220.5 2.5v2M227 5l-1.5 1.5" stroke="white" />
         <circle cx={220.5} cy={11.5} r={4} />
-        <path d="m214 5 1.5 1.5M220.5 20.5v-2M227 18l-1.5-1.5M214 18l1.5-1.5M211.5 11.5h2M227.5 11.5h2" />
+        <path
+          d="m214 5 1.5 1.5M220.5 20.5v-2M227 18l-1.5-1.5M214 18l1.5-1.5M211.5 11.5h2M227.5 11.5h2"
+          stroke="white"
+        />
       </g>
     </g>
   </svg>
-);
+)
 
-export default Sun;
+export default Sun
