23f326a7fb41af0f9862ebdbb12da810c4eb65dd
Lucas Pirez <lucaspirez42@gmail.com>
2024-11-29 15:09:11 -0300

    this commit will add changes in projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -38,91 +38,48 @@ export default function Projects() {
       id="portfolio"
       className="relative flex flex-col  max-w-[1500px] m-auto min-h-[100vh] items-center border-t-2 border-t-myDarkLightBlue/70 background_black  pt-[10vh] dark:bg-myBgDark   bg-myBgLight shadow-md shadow-zinc-400 z-10 -mt-[10vh] md:mt-0"
     >
-      <>
-        <Title text={text.projects} />
-        <ProjectsTecnologies
-          onSelect={onSelect}
-          select={select}
-          text={text}
-        />
-        <div className="flex flex-col items-center relative h-[75vh] overflow-y-scroll   sm:overflow-y-auto sm:h-auto md:w-[90vw] w-[95vw] scroll_style">
-          <div
-            ref={ref}
-            className="flex w-full relative  justify-center flex-wrap container max-w-[1180px] gap-[0.5vw] "
-          >
-            <ContainerProjects
-              select={select}
-              number={[3]}
-              imgSrc={speedyGame}
-              title=".NET/React"
-              links={text.modal['typingGame']}
-            >
-              <ButtonRead
-                handleClick={(e) => {
-                  e.preventDefault();
-                  e.stopPropagation();
-                  handleClick('typingGame');
-                }}
-              />
-            </ContainerProjects>
-            <ContainerProjects
-              select={select}
-              number={[3]}
-              imgSrc={cryptoMain}
-              title=".Net/React"
-              links={text.modal['cryptoTracker']}
-            >
-              <ButtonRead
-                handleClick={(e) => {
-                  e.preventDefault();
-                  e.stopPropagation();
-                  handleClick('cryptoTracker');
-                }}
-              />
-            </ContainerProjects>
-            <ContainerProjects
-              select={select}
-              number={[2]}
-              imgSrc={AddStudent}
-              title="Institute App"
-              links={text.modal['institute']}
-            >
-              <ButtonRead
-                handleClick={(e) => {
-                  e.preventDefault();
-                  e.stopPropagation();
-                  handleClick('institute');
-                }}
-              />
-            </ContainerProjects>
+      <Title text={text.projects} />
+      <ProjectsTecnologies
+        onSelect={onSelect}
+        select={select}
+        text={text}
+      />
+      <div className="flex justify-center relative h-[75vh] overflow-y-scroll   sm:overflow-y-auto sm:h-auto md:w-[90vw] w-[95vw] scroll_style">
+        <div
+          ref={ref}
+          className="grid grid-cols-1 sm:grid-cols-2 items-center w-full grid-flow-row relative flex-wrap container max-w-[1180px] gap-[1rem]"
+        >
+          <ContainerProjects
+            select={select}
+            number={[3]}
+            imgSrc={speedyGame}
+            title=".NET/React"
+            links={text.modal['typingGame']}
+          />
+          <ContainerProjects
+            select={select}
+            number={[3]}
+            imgSrc={cryptoMain}
+            title=".Net/React"
+            links={text.modal['cryptoTracker']}
+          />
+          <ContainerProjects
+            select={select}
+            number={[2]}
+            imgSrc={AddStudent}
+            title="Institute App"
+            links={text.modal['institute']}
+          />
 
-            <ContainerProjects
-              select={select}
-              number={[]}
-              imgSrc={twitter}
-              title="Reactjs"
-              links={text.modal['tweeter']}
-            >
-              <ButtonRead
-                handleClick={(e) => {
-                  e.preventDefault();
-                  e.stopPropagation();
-                  handleClick('tweeter');
-                }}
-              />
-            </ContainerProjects>
-          </div>
-
-          {close && (
-            <ModalPortal>
-              <ModalChildren
-                modalSelect={modalSelect}
-                outside={outside}
-              />
-            </ModalPortal>
-          )}
+          <ContainerProjects
+            select={select}
+            number={[]}
+            imgSrc={twitter}
+            title="Reactjs"
+            links={text.modal['tweeter']}
+          />
         </div>
-      </>
+      </div>
     </div>
   );
 }
