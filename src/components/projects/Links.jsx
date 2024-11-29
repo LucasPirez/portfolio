6faf3e521a6dd254133efd9dbfb6f35cf3abbfa1
Lucas Pirez <lucaspirez42@gmail.com>
2024-11-29 19:46:55 -0300

    This commit will add changes in Likns projects

--- src/components/projects/Links.jsx
+++ src/components/projects/Links.jsx
@@ -1,21 +1,32 @@
 import externalLink from '../../icons/external-link.svg';
 import gitHub from '../../icons/433-github.svg';
 
-export default function Links({ links }) {
+export default function Links({
+  links,
+  description,
+  title,
+  tecnologies,
+}) {
   return (
-    <>
-      <a href={links.repositorie} target="_blank" rel="noreferrer">
-        <img src={gitHub} alt="git" className="hover:scale-105" />
-      </a>
-      {links?.deploy && (
-        <a href={links.deploy} target="_blank" rel="noreferrer">
+    <div className=" flex justify-end w-full  absolute right-1 top-1 z-10">
+      <div className=" flex bg-slate-200 p-[3px] rounded-md ">
+        <a href={links.repositorie} target="_blank" rel="noreferrer">
           <img
-            src={externalLink}
-            alt="link"
-            className="hover:scale-110 transition-all"
+            src={gitHub}
+            alt="git"
+            className="cursor-pointer hover:scale-110 "
           />
         </a>
-      )}
-    </>
+        {links?.deploy && (
+          <a href={links.deploy} target="_blank" rel="noreferrer">
+            <img
+              src={externalLink}
+              alt="link"
+              className="hover:scale-110 transition-all"
+            />
+          </a>
+        )}
+      </div>
+    </div>
   );
 }
