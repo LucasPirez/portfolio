09c9a4ed47810b72aaafab24e3dc1cf4ba62ff05
Lucas Pirez <lucaspirez42@gmail.com>
2024-12-04 17:22:25 -0300

    This commit will fix bug in container projects

--- src/components/projects/ContainerProjects.jsx
+++ src/components/projects/ContainerProjects.jsx
@@ -6,11 +6,13 @@ function ContainerProjects({ imgSrc, text, tecnologies }) {
   return (
     <article className=" flex justify-end flex-col rounded-lg relative bg-zinc-900 border-[1px] border-purple/40 w-[100%] max-w-[500px] h-[460px] overflow-hidden  dark:opacity-90 hover:opacity-100 transition-all duration-150 hover:scale-[1.03]  ">
       <Links links={text} />
-      <img
-        src={imgSrc}
-        className=" absolute top-0 left-0 object-cover h-full from-transparent "
-        alt="project"
-      />
+      <div className="w-full h-full absolute top-0 left-0 flex justify-center">
+        <img
+          src={imgSrc}
+          className=" object-cover h-full"
+          alt="project"
+        />
+      </div>
 
       <div className="h-full w-full bg-gradient-to-b dark:to-zinc-900 from-transparent absolute bottom-0 left-0 bg-slate-700/20 opacity-50 hover:opacity-30 "></div>
       <div className="z-20 pl-3 pr bg-myBgDarkSecondary ">
