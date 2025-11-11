dcd72516258f241c4f14f42df8c88bb3ac041509
Lucas Pirez <lucaspirez42@gmail.com>
2025-11-11 11:35:20 -0300

    Delete and replace Images

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,37 +1,16 @@
 import speedyRoom from './typing-game/speedy-room.png';
 import speedyHome from './typing-game/speedy-home.png';
 import speedyWinner from './typing-game/speedy-room-winner.png';
+import mechanic from './mecanica.webp';
+import motorbike from './motorbike-customer.webp';
 
 import grinpol from './grinpol.png';
-import cryptoMain from './cripto/img-crypto-main.webp';
-import cryptoMobile from './cripto/img-crypto-mobile.webp';
-import cryptoCoin from './cripto/img-cypto-coin.webp';
-
-import mobileChart from './cripto/mobile-chart.webp';
-import mobileChartLineal from './cripto/mobile-chartLineal.webp';
-import mobilecrypto from './cripto/mobile-crypto.webp';
-import mobileTable from './cripto/mobile-table.webp';
-
-// institute
-import AddStudent from './institute/agregar-estudiante.webp';
-import Configuration from './institute/configuracion.webp';
-import Debts from './institute/deudores.webp';
-import PaidFee from './institute/pago-couta.webp';
 
 export {
   speedyRoom,
   speedyHome,
   speedyWinner,
   grinpol as tweeter1,
-  cryptoMain,
-  cryptoMobile,
-  cryptoCoin,
-  mobileChart,
-  mobileChartLineal,
-  mobilecrypto,
-  mobileTable,
-  AddStudent,
-  Configuration,
-  Debts,
-  PaidFee,
+  mechanic,
+  motorbike,
 };
